dd9b78a0bf486641652ba9902504a6e85ff6ac8b
DT128 <53341243+DT128@users.noreply.github.com>
2020-02-23 14:16:06 +0530

    To find Factorial of any number by recurssion.

---
+++ factorial.c
@@ -0,0 +1,47 @@
+/*
+ ============================================================================
+ Name        : .c
+ Author      : Dhirendra Tiwari
+ Version     :
+ Copyright   : Your copyright notice
+ Description :  in C
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int fact(int num1)
+{
+    int num2 = 1;
+   // printf("Enter No. to find its factorial : ");
+    //scanf("%d",&num1);
+   // printf("Factorial of %d is : ",num1);
+    while(num1>=1)
+    {
+        num2 *= num1 ;
+        num1--;
+    }
+    return num2;
+}
+int  comb()
+{
+    int n,c,r;
+    printf("Enter value of n : ");
+    scanf("%d",&n);
+    printf("Enter value of r : ");
+    scanf("%d",&r);
+    printf("Combination %dC%d is : ",n,r);
+    c = fact(n)/(fact(r)*fact(n-r));
+    return c;
+
+}
+
+int main()
+{
+   // printf("%d",fact());
+    printf("%d",comb());
+
+		return EXIT_SUCCESS;
+}
+
